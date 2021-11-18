fe9e5bb0d48f114d52211d4c19abfff672d3a725
niemax <axel.nieminen@mac.com>
2021-11-18 17:28:41 +0200

    story update

--- src/components/UI/Atoms/Story.tsx
+++ src/components/UI/Atoms/Story.tsx
@@ -10,11 +10,11 @@ export const Story: React.FC<StoryProps> = ({}) => {
       <Heading>What's my story?</Heading>
       <LineBreak width="15rem" mb={5} />
       <Heading size="md">
-        Jumped into the world of Information Technology in 2019 as a wandered. Ever since it has
-        been the one thing that I'm super passionate about and want to improve myself at a
-        never-ending pace. Always looking for new creative ways to provide accessible and smooth
-        software for all kinds of people. Love working as a team and taking into account everyones'
-        vision. Also constantly trying to better myself on clean code principles.
+        Jumped into the world of Information Technology in 2019. Ever since it has been the one
+        thing that I'm super passionate about and want to improve myself at a never-ending pace.
+        Always looking for new creative ways to provide accessible and smooth software for all kinds
+        of people. Love working as a team and taking into account everyones' vision. Also constantly
+        trying to better myself on clean code principles.
       </Heading>
     </>
   );
