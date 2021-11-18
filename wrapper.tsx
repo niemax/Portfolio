ab6e7fdd8390a1a9da3547628bc7f076bb3c3105
niemax <axel.nieminen@mac.com>
2021-11-18 12:27:59 +0200

    navbar and footer fix attempt #2

--- wrapper.tsx
+++ wrapper.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
-import Layout from './src/components/UI/layout';
-import theme from './src/gatsby-plugin-chakra-ui/theme';
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "./src/components/UI/layout";
+import theme from "./src/gatsby-plugin-chakra-ui/theme";
 
 export const wrapPageElement = ({ element }) => {
   return (
-    <ChakraProvider resetCSS theme={theme}>
+    <ChakraProvider theme={theme}>
       <Layout>{element}</Layout>
     </ChakraProvider>
   );
