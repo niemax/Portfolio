da974f404e03b588975eaae89907201481e15db7
niemax <axel.nieminen@mac.com>
2021-11-19 20:31:19 +0200

    color change boxes

--- src/theme/foundations/colors.ts
+++ src/theme/foundations/colors.ts
@@ -2,7 +2,7 @@ export const colors = {
   green: "#00A375",
   gray: "#E8E8E8",
   dark: "#172E35",
-  boxes: "#172E35",
+  boxes: "#1C2F35",
   navLightHover: "#E0E0E0",
   navDarkHover: "#151B23",
   yellow: "#A68811",
