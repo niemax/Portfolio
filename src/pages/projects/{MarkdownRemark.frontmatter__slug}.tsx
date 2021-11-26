ac91738eff937e2ee9fa7c7adce23a60b69241b8
niemax <axel.nieminen@mac.com>
2021-11-26 20:29:06 +0200

    added page enter animation to markdown pages

--- src/pages/projects/{MarkdownRemark.frontmatter__slug}.tsx
+++ src/pages/projects/{MarkdownRemark.frontmatter__slug}.tsx
@@ -12,6 +12,7 @@ import {
 import { FaGithub } from "react-icons/fa";
 import Seo from "../../components/seo";
 import { windowOpen } from "../../utility/helpers/windowOpen";
+import { PageScaleFade } from "../../components/motion/transitions";
 
 function ProjectDetailsTemplate({ data: { markdownRemark } }: any) {
   const { frontmatter, html } = markdownRemark;
@@ -19,37 +20,38 @@ function ProjectDetailsTemplate({ data: { markdownRemark } }: any) {
   return (
     <Flex px={[4, 14, 14, 14]} direction="column">
       <Seo title={frontmatter.title} />
-
-      <Box align="center" bg={"green"} py={6} rounded="xl" shadow="md">
-        <AspectRatio maxW={640} ratio={1}>
-          <iframe
-            src={frontmatter.videoURL}
-            loading="lazy"
-            webkitallowfullscreen={true}
-            mozallowfullscreen={true}
-            allowFullScreen={true}
-          />
-        </AspectRatio>
-      </Box>
-      <Box mt={8} w="100%" h={"0.1rem"} bg="grey" />
-      <Box mt={10}>
-        <HStack justify="space-between" align="center" mb={6}>
-          <Heading fontWeight="700">{frontmatter.title}</Heading>
-          <HStack>
-            <Heading size="sm">Project Link</Heading>
-            <IconButton
-              as="a"
-              fontSize={26}
-              variant="ghost"
-              aria-label="Github Link"
-              icon={<FaGithub />}
-              _hover={{ bg: "#6e5494" }}
-              onClick={() => windowOpen(frontmatter.gitHubLink)}
+      <PageScaleFade>
+        <Box align="center" bg={"green"} py={6} rounded="xl" shadow="md">
+          <AspectRatio maxW={640} ratio={1}>
+            <iframe
+              src={frontmatter.videoURL}
+              loading="lazy"
+              webkitallowfullscreen={true}
+              mozallowfullscreen={true}
+              allowFullScreen={true}
             />
+          </AspectRatio>
+        </Box>
+        <Box mt={8} w="100%" h={"0.1rem"} bg="grey" />
+        <Box mt={10}>
+          <HStack justify="space-between" align="center" mb={6}>
+            <Heading fontWeight="700">{frontmatter.title}</Heading>
+            <HStack>
+              <Heading size="sm">Project Link</Heading>
+              <IconButton
+                as="a"
+                fontSize={26}
+                variant="ghost"
+                aria-label="Github Link"
+                icon={<FaGithub />}
+                _hover={{ bg: "#6e5494" }}
+                onClick={() => windowOpen(frontmatter.gitHubLink)}
+              />
+            </HStack>
           </HStack>
-        </HStack>
-      </Box>
-      <Box dangerouslySetInnerHTML={{ __html: html }} />
+        </Box>
+        <Box dangerouslySetInnerHTML={{ __html: html }} />
+      </PageScaleFade>
     </Flex>
   );
 }
