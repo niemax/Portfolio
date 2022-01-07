584b9da820873302d51acd7e7982c59e85135f2a
niemax <axel.nieminen@mac.com>
2022-01-07 19:21:37 +0200

    added boxes to projects description

--- src/components/UI/Atoms/ProjectCards.tsx
+++ src/components/UI/Atoms/ProjectCards.tsx
@@ -9,6 +9,7 @@ import {
   Box,
   useColorModeValue,
   IconButton,
+  Flex,
 } from "@chakra-ui/react";
 import { MBox } from "../../motion/MotionBox";
 import { container, fromDownToUpItem, item, otherItem } from "../../motion/transitions";
@@ -58,7 +59,7 @@ export const ProjectCards = (props: IProjectCardProps) => {
               rounded={props.rounded}
               _hover={{ opacity: 1 }}
               animation={props.animation}
-              opacity={0.8}
+              opacity={0.85}
               mt={props.mt || 20}
               mb={20}
               padding={props.padding}
@@ -67,69 +68,71 @@ export const ProjectCards = (props: IProjectCardProps) => {
               border={props.border}
             >
               <motion.div variants={otherItem}>
-                <MBox width="auto" height="auto" animation={{ scale: 1.03 }} shadow="2xl">
+                <MBox width="auto" height="auto" animation={{ scale: 1.01 }} shadow="2xl">
                   <GatsbyImage image={image} />
                 </MBox>
               </motion.div>
               <motion.div variants={fromDownToUpItem}>
-                <VStack>
-                  <Box>
-                    <HStack>
-                      <Link to={resolvePathToSlug(props.slug, node.slug)}>
-                        <Heading
-                          textDecor="none"
-                          mb={1}
-                          size={props.headingSize}
-                          fontWeight={props.headingWeight}
-                        >
-                          {node.name}
-                        </Heading>
-                      </Link>
-                    </HStack>
-                    <Box maxW={80} mx="auto" mt={3}>
-                      <Heading size="sm" color={props.textColor}>
-                        {node.description}
-                      </Heading>
-                    </Box>
-                    <HStack mt={props.mt || 5} mb={2}>
-                      {node.tech.map((itm: any, idx: string) => (
-                        <i>
-                          <Heading
-                            key={idx}
-                            size="sm"
-                            color={useColorModeValue("black", "white")}
-                            opacity={0.8}
-                            fontWeight={700}
-                          >
-                            {itm}
-                          </Heading>
-                        </i>
-                      ))}
-                    </HStack>
-                    <Box align={index % 2 !== 0 && "right"}>
-                      <Tooltip
-                        placement="auto"
-                        bg={useColorModeValue("black", "white")}
-                        color={useColorModeValue("white", "black")}
-                        aria-label="Project link"
-                        label="GitHub link"
-                      >
-                        <IconButton
-                          mt={3}
-                          isRound={true}
-                          size="md"
-                          onClick={() => windowOpen(node.url)}
-                          variant="outline"
-                          colorScheme="teal"
-                          aria-label="project link"
-                          fontSize="20px"
-                          shadow="md"
-                          icon={<RiShareBoxFill />}
-                        />
-                      </Tooltip>
-                    </Box>
+                <Box
+                  mt={24}
+                  p={3}
+                  rounded="lg"
+                  shadow="xl"
+                  bg={useColorModeValue("light", "boxes")}
+                >
+                  <Link to={resolvePathToSlug(props.slug, node.slug)}>
+                    <Heading
+                      textDecor="none"
+                      mb={1}
+                      size={props.headingSize}
+                      fontWeight={props.headingWeight}
+                    >
+                      {node.name}
+                    </Heading>
+                  </Link>
+                  <Box w={80} mx="auto" mt={3}>
+                    <Heading size="sm" color={props.textColor}>
+                      {node.description}
+                    </Heading>
                   </Box>
-                </VStack>
+                  <HStack mt={props.mt || 5} mb={2}>
+                    {node.tech.map((itm: any, idx: string) => (
+                      <i>
+                        <Heading
+                          key={idx}
+                          size="sm"
+                          color={useColorModeValue("black", "white")}
+                          opacity={0.8}
+                          fontWeight={700}
+                        >
+                          {itm}
+                        </Heading>
+                      </i>
+                    ))}
+                  </HStack>
+                  <Box align={index % 2 !== 0 && "right"}>
+                    <Tooltip
+                      placement="auto"
+                      bg={useColorModeValue("black", "white")}
+                      color={useColorModeValue("white", "black")}
+                      aria-label="Project link"
+                      label="GitHub link"
+                    >
+                      <IconButton
+                        mt={3}
+                        isRound={true}
+                        size="md"
+                        onClick={() => windowOpen(node.url)}
+                        variant="outline"
+                        colorScheme="teal"
+                        aria-label="project link"
+                        fontSize="20px"
+                        shadow="md"
+                        icon={<RiShareBoxFill />}
+                      />
+                    </Tooltip>
+                  </Box>
+                </Box>
               </motion.div>
             </Stack>
           </motion.div>
