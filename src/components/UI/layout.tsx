29866bdfa57271cfd5b49604d95a7f4ee27e1bfa
niemax <axel.nieminen@mac.com>
2022-01-25 17:51:30 +0200

    removed welcome animation and changed redux skill to 1 level

--- src/components/UI/layout.tsx
+++ src/components/UI/layout.tsx
@@ -1,37 +1,12 @@
 import React, { ReactNode, Fragment } from "react";
 import Header from "./header";
 import { Footer } from "./footer";
-import { Container, Flex, Heading } from "@chakra-ui/react";
-import { motion } from "framer-motion";
-import { container, item, otherItem } from "../motion/transitions";
 
 interface ILayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: ILayoutProps) => {
-  const [showWelcomeText, setShowWelcomeText] = React.useState<boolean>(false);
-
-  React.useEffect(() => {
-    setShowWelcomeText(true);
-
-    const timeout = setTimeout(() => {
-      setShowWelcomeText(false);
-    }, 2000);
-
-    return () => clearTimeout(timeout);
-  }, []);
-
-  if (showWelcomeText)
-    return (
-      <Flex align="center" justify="center" mt="50vh">
-        <motion.div variants={container} initial="hidden" animate="show">
-          <motion.div variants={item}>
-            <Heading textDecor="none">Welcome!</Heading>
-          </motion.div>
-        </motion.div>
-      </Flex>
-    );
   return (
     <Fragment>
       <Header />
